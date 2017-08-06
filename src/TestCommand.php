7e5f3ab9ff558d36bb05a748c07a7baf94726fec
Alain Schlesser <alain.schlesser@gmail.com>
2017-08-06 14:24:44 +0200

    Initial commit.

---
+++ src/TestCommand.php
@@ -0,0 +1,14 @@
+<?php namespace WP_CLI_Test\TestCommand;
+
+class TestCommand {
+
+	/**
+	 * Generate a version string to test against.
+	 *
+	 * @command test-command
+	 * @when before_wp_load
+	 */
+	public function __invoke() {
+		\WP_CLI::success( 'Version A.' );
+	}
+}
