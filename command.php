144a7ba7ee27f9439dac4a0085c7c51754fa29a8
Alain Schlesser <alain.schlesser@gmail.com>
2017-08-06 14:24:44 +0200

    Initial commit.

---
+++ command.php
@@ -0,0 +1,15 @@
+<?php namespace AlainSchlesser\TestCommand;
+
+if ( ! defined( 'WP_CLI' ) ) {
+	return;
+}
+
+$autoloader = __DIR__ . '/vendor/autoload.php';
+if ( is_readable( $autoloader ) ) {
+	include $autoloader;
+}
+
+\WP_CLI::add_command(
+	'test-command',
+	TestCommand::class
+);
---
+++ src/TestCommand.php
@@ -0,0 +1,14 @@
+<?php namespace AlainSchlesser\TestCommand;
+
+class TestCommand {
+
+	/**
+	 * Generate a version string to test against.
+	 *
+	 * @command test-command
+	 * @when before_wp_load
+	 */
+	public function __invoke() {
+		\WP_CLI::success( 'Version A.' );
+	}
+}
